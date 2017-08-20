697bb07884ea79141790c6e8a5eaebf81b8c1243
jazlemail-velociraptor <jazlemail-velociraptor@yahoo.com>
2017-08-19 18:27:35 -0700

    opening an existing file with the file editor!!

--- src/com/azl/AnalysisToolWindowFactory.java
+++ src/com/azl/AnalysisToolWindowFactory.java
@@ -2,20 +2,47 @@ package com.azl;
 
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileSystem;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.util.messages.MessageBus;
+import org.jdesktop.swingx.action.ActionManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class AnalysisToolWindowFactory implements ToolWindowFactory {
 
     JLabel messageLabel;
+
+    private void createNewFile() {
+        Project project = ProjectManager.getInstance().getOpenProjects()[0];
+
+        final String testFilePath = "D:\\work\\intellij-plugin\\plugins\\tw\\src\\com\\azl\\ShowTw.java";
+        final VirtualFile testFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(new File(testFilePath));
+
+        LocalFileSystem fileSystem = LocalFileSystem.getInstance();
+        OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, testFile);
+
+        FileEditorManager fem = FileEditorManager.getInstance(project);
+        fem.openEditor(openFileDescriptor, true);
+    }
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
@@ -31,6 +58,16 @@ public class AnalysisToolWindowFactory implements ToolWindowFactory {
         c.gridy = c.gridy+1;
         messageLabel = new JLabel("Messages go here!");
         panel.add(messageLabel, c);
+
+        c.gridy = c.gridy+1;
+        JButton newFileButton = new JButton("New File");
+        newFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createNewFile();
+            }
+        });
+        panel.add(newFileButton, c);
 
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(panel,"",false);
