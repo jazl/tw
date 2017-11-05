d7d1b5cf783092afc9e9973438f4f7da7505e408
jazl <alan_laya@hotmail.com>
2017-11-05 12:56:18 -0800

    test HTML content and links in JLabels

--- src/com/fortify/fod/remediation/standalone/HtmlLabelTests.java
+++ src/com/fortify/fod/remediation/standalone/HtmlLabelTests.java
@@ -12,7 +12,7 @@ import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.StyleSheet;
 
-public class Main extends JFrame {
+public class HtmlLabelTests extends JFrame {
 
     StyleSheet styleSheet = new StyleSheet();
     HTMLDocument htmlDocument;
@@ -20,13 +20,13 @@ public class Main extends JFrame {
     Element bodyElement;
 
     public static void main(String[] args) throws Exception {
-        Main jTextPaneApp = new Main();
+        HtmlLabelTests jTextPaneApp = new HtmlLabelTests();
         jTextPaneApp.setVisible(true);
 //        Thread.currentThread().sleep(1000);
 //        jTextPaneApp.change();
     }
 
-    public Main() {
+    public HtmlLabelTests() {
         setSize(400, 400);
         styleSheet.addRule(".someclass1 {color: blue;}");
         styleSheet.addRule(".someclass2 {color: green;}");
@@ -48,9 +48,13 @@ public class Main extends JFrame {
             addContent("<span class=someclass1>test 1</span><br>");
             addContent("<span class=someclass2>test 2</span><br>");
 
+            JLabel linkLabel = new JLabel("<html><body><a>www.msn.com</a></body></html>");
+            contentPane.add(linkLabel, BorderLayout.SOUTH);
+
             JPanel panel = new JPanel();
             panel.add(new JLabel("<html><h3>JLabel</h3><span class=someclass2>test 2</span><br></html>"));
             contentPane.add(panel, BorderLayout.CENTER);
+
 
         } catch (Exception e) {
             e.printStackTrace();
